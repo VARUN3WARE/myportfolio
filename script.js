97600c2c504460b569f3e2622398bb64794bf8e0
Varun Rao <106867498+VARUN3WARE@users.noreply.github.com>
2025-03-31 13:03:23 +0530

    Update script.js
    addded new links

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('user-query').value = '';
 
         // Make a POST request to the Flask API
-        const response = await fetch('http://127.0.0.1:5000/ask', {
+        const response = await fetch('https://protfolio-chatbot.vercel.app/ask', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
